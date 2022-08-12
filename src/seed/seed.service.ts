13277ba2f86c6e789e92f870be404458e9932314
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-08-12 16:40:29 -0500

    files

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ProductService } from '../product/product.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
   constructor(private readonly productServices: ProductService) {}
   async runSeed() {
     await this.insertNewProducts();
-    return 'This action adds a new seed';
+    return 'SEED EXECUTED';
   }
 
   private async insertNewProducts() {
     await this.productServices.deleteAllProducts();
+
+    const productos = initialData.products;
+
+    const insertPromises = [];
+
+    productos.forEach((product) => {
+      insertPromises.push(this.productServices.create(product));
+    });
+
+    await Promise.all(insertPromises);
+
     return true;
   }
 }
